17da872be9f27080ea441392620bdfcd527780d0
Jonathan Jaco <jacojonathan12@gmail.com>
2021-05-16 19:26:16 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,52 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(255,192,203);
+  textStyle(BOLD);
+  textSize(25);
+  fill(0);
+  textAlign(CENTER);
+  text("Press any Key to Start", windowWidth/2, windowHeight / 2);
+}
+function roulette() {
+  
+  var num = int(random(0, 38));
+  let listBlacks = [10,29,8,31,6,33,4,35,2,28,26,11,20,17,22,15,21,13];
+  // let colour = ['red', 'blue'];
+  let colour;
+  if (num == 0 || num == 37) {
+    num = 0; 
+    colour = 'green'; 
+  }
+  else {
+      for (let i = 0; i < listBlacks.length; i++) {
+        if (num == listBlacks[i]) {
+          colour = 'black'
+          break;
+        }
+        colour = 'red'
+      }
+}
+
+  if (colour == 'red') {
+    background(255, 0, 0);
+  } 
+  else if (colour == 'green'){
+    background(0,255,0);
+  }
+  else {
+    background(0,0,0);
+  }
+  textStyle(BOLD);
+  textSize(100);
+  fill(255);
+  textAlign(CENTER);
+  text(num, windowWidth / 2, windowHeight / 2);
+}
+
+function draw() {
+
+}
+
+function keyReleased() {
+  roulette(); 
+}
